5d5afc053e82b98d6e352c0c3a7fb80267eed70a
Ioannis <glaventzakis@gmail.com>
2025-05-02 12:28:28 +0200

    MenuItem updated

--- src/main/java/com/kouzina/kouzina/controller/MenuController.java
+++ src/main/java/com/kouzina/kouzina/controller/MenuController.java
@@ -1,4 +1,37 @@
 package com.kouzina.kouzina.controller;
 
+import com.kouzina.kouzina.model.MenuItem;
+import com.kouzina.kouzina.repository.MenuItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class MenuController {
+
+    @Autowired
+    private MenuItemRepository menuItemRepository;
+
+    @GetMapping("/menu")
+    public String showMenu(Model model) {
+        model.addAttribute("items", menuItemRepository.findAll());
+        return "menu";
+    }
+
+    @GetMapping("/menu/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("menuItem", new MenuItem());
+        return "add-menu-item";
+    }
+
+    @PostMapping("/menu/add")
+    public String addMenuItem(@ModelAttribute MenuItem menuItem) {
+        menuItemRepository.save(menuItem);
+        return "redirect:/menu";
+    }
+    @GetMapping("/")
+    public String showHome() {
+        return "redirect:/menu";
+    }
 }
